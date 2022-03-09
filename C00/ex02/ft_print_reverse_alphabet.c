2688da46f80c99a6a7fbac2bc22e2a29f13d32b1
Luke MONTGOMERY <lmontgom@e3r2p4.clusters.42paris.fr>
2022-03-09 19:54:14 +0100

    1st

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmontgom <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/09 16:02:04 by lmontgom          #+#    #+#             */
+/*   Updated: 2022/03/09 16:16:34 by lmontgom         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	c;
+
+	c = 'z';
+	while (c >= 'a')
+	{
+		write(1, &c, 1);
+		c--;
+	}
+}
+
+void	main(void)
+{
+	ft_print_reverse_alphabet();
+}
